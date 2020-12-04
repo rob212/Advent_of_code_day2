7892156abcdf3a798e1b526219b3318b00f8ef84
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-12-04 17:48:35 +0000

    better output from the main function

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import { passwordValidator, isPasswordValid as validatorOne } from './PasswordVa
 import { isPasswordValid as validatorTwo } from './PasswordValidatorPart2'
 
 const resultChallengeOne = passwordValidator('test_data.txt', validatorOne)
-console.log(`The number of validate passwords in the file is: ${resultChallengeOne}`)
+console.log(`Challenge 1: The number of validate passwords in the file is: ${resultChallengeOne}`)
 
 const resultChallengeTwo = passwordValidator('test_data.txt', validatorTwo)
-console.log(`The number of validate passwords in the file is: ${resultChallengeTwo}`)
+console.log(`Challenge 2: The number of validate passwords in the file using the new validator is: ${resultChallengeTwo}`)
