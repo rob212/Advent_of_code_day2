50402992fea428dc7376c86a10b06d157b7797e9
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-12-07 09:06:27 +0000

    removal of excessive passing of argument where a default parameter is used

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
-import { passwordValidator, isPasswordValid as validatorOne } from './PasswordValidator'
-import { isPasswordValid as validatorTwo } from './PasswordValidatorPart2'
+import { passwordValidator } from './PasswordValidator'
+import { isPasswordValid as challengeTwoValidator } from './PasswordValidatorPart2'
 
-const resultChallengeOne = passwordValidator('test_data.txt', validatorOne)
+const resultChallengeOne = passwordValidator('test_data.txt')
 console.log(`Challenge 1: The number of validate passwords in the file is: ${resultChallengeOne}`)
 
-const resultChallengeTwo = passwordValidator('test_data.txt', validatorTwo)
+const resultChallengeTwo = passwordValidator('test_data.txt', challengeTwoValidator)
 console.log(`Challenge 2: The number of validate passwords in the file using the new validator is: ${resultChallengeTwo}`)
